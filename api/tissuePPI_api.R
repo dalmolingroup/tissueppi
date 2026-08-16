a75632363eeca0a56b099e590ef065e0123a6a05
Fábio Silva <fabiohfs@yahoo.com.br>
2026-08-16 14:58:05 -0300

    Configuração inicial do diretório do projeto.

---
+++ api/tissuePPI_api.R
@@ -0,0 +1,220 @@
+# plumber.R
+#library(vroom)
+library(dplyr)
+library(magrittr)
+library(DBI)
+library(RSQLite)
+library(glue)
+library(igraph)
+library(pool)
+
+# 1. Pool criado para gerenciar as conexões com o banco (inicia quando a API sobe no servidor)
+#pool <- dbPool(
+#  drv = RSQLite::SQLite(),
+#  dbname = "/app/data/interactions.sqlite",
+#  onCreate = function(con) {
+#    dbExecute(con, "PRAGMA journal_mode = WAL;")     # Permite leitura e escrita simultânea
+#    dbExecute(con, "PRAGMA synchronous = NORMAL;")   # Reduz a frequência de gravação no disco
+#    dbExecute(con, "PRAGMA cache_size = -131072;")   # 128MB
+#    dbExecute(con, "PRAGMA mmap_size = 1073741824;") # 1GB
+#    dbExecute(con, "PRAGMA busy_timeout = 5000;")    # Evita travamento da aplicação em caso de tentativa de escrita simultânea
+#  }
+#)
+
+# Garante que o pool feche quando o servidor Plumber for encerrado
+#*@plumber
+function(pr) {
+  pr$registerHook("exit", function() {
+    poolClose(pool)
+  })
+}
+db_name <- "/app/data/interactions.sqlite"
+
+database_connect <- function() {
+  con <- dbConnect(RSQLite::SQLite(), db_name)
+  
+  # Pragmas essenciais para ambientes web concorrentes
+  dbExecute(con, "PRAGMA journal_mode = WAL;")
+  dbExecute(con, "PRAGMA synchronous = NORMAL;")
+  dbExecute(con, "PRAGMA busy_timeout = 5000;") # Espera até 5s se o DB estiver ocupado
+  dbExecute(con, "PRAGMA foreign_keys = ON;")
+  dbExecute(con, "PRAGMA cache_size = -131072;")   # 128MB
+  dbExecute(con, "PRAGMA mmap_size = 3221225472;") # 3GB
+  
+  return(con)
+}
+
+extract_subgraph_by_degree <- function(edges, seed_protein, layer) {
+  if (nrow(edges) == 0 || !seed_protein %in% unique(c(edges$from, edges$to))) {
+    return(data.frame(from = character(), to = character()))
+  }
+  
+  # Criar o grafo não direcionado
+  g <- graph_from_data_frame(edges, directed = FALSE)
+  # Remover arestas duplicadas e loops
+  g <- simplify(g)
+  
+  # Encontrar todos os vértices até o grau especificado
+#  if (degree > 0) {
+    # Obter distâncias do vértice inicial
+#    distances <- distances(g, v = seed_protein)
+    
+    # Filtrar vértices dentro do grau especificado
+#    valid_vertices <- names(which(distances[1, ] <= degree & distances[1, ] > 0))
+#    valid_vertices <- c(seed_protein, valid_vertices)
+  if (seed_protein %in% V(g)$name) {
+    
+    # Descobrir o ID numérico ou nome do vértice 'palio'
+    v_protein <- seed_protein
+    
+    # Obter todos os nós vizinhos até a distância 2
+    valid_vertices <- neighborhood(g, order = layer, nodes = v_protein)[[1]]    
+
+    # Extrair o subgrafo induzido
+    if (length(valid_vertices) > 1) {
+      subg <- induced_subgraph(g, valid_vertices)
+      subg_edges <- as_data_frame(subg)
+      return(subg_edges)
+    } else {
+      return(data.frame(from = character(), to = character()))
+    }
+  } else {
+    # Grau 0: apenas o nó inicial
+    return(data.frame(from = character(), to = character()))
+  }
+}
+
+
+#* Gerar a lista de códigos de proteinas
+#* @get /protein_list
+function() {
+  con <- database_connect()
+  on.exit(dbDisconnect(con), add = TRUE) # Garante o fechamento seguro da conexão
+  query <- glue_sql("SELECT geneSymbol, idProtein FROM proteins ORDER BY geneSymbol", .con = con)
+  protein_list <- dbGetQuery(con, query)
+  return(protein_list)
+}
+
+#* Gerar a lista de códigos dos tecidos
+#* @get /tissue_list
+function() {
+  con <- database_connect()
+  on.exit(dbDisconnect(con), add = TRUE) # Garante o fechamento seguro da conexão
+  query <- glue_sql("SELECT idTissue,descTissue FROM tissues ORDER BY descTissue", .con = con)
+  tissue_list <- dbGetQuery(con, query)
+  
+  return(tissue_list)
+}
+
+#* Baixar o banco todo ou a rede de um tecido
+#* @get /download_edges
+function(sqltxt) {
+  con <- database_connect()
+  on.exit(dbDisconnect(con), add = TRUE) # Garante o fechamento seguro da conexão
+  query <- glue_sql({sqltxt}, .con = con)
+  edges_list <- dbGetQuery(con, query)
+  return(edge_list)
+}
+
+#* Receber uma lista de proteínas e tecido, e retornar subconjunto para criar o grafo
+#* @param genes A lista de genes (separados por virgula)
+#* @param tissue O tecido
+#* @post /multiple_query
+function(genes, tissue) {
+  con <- database_connect()
+  on.exit(dbDisconnect(con), add = TRUE) # Garante o fechamento seguro da conexão
+  genes_list <- unlist(strsplit(genes, ",")) 
+  genes_list <- trimws(genes_list)
+    query <- glue_sql(
+    "SELECT geneSymbol1, geneSymbol2, idTissue
+     FROM interactions
+     WHERE idTissue = {tissue} 
+     AND (geneSymbol1 IN ({genes_list*}) AND geneSymbol2 IN ({genes_list*}))",
+    .con = con
+  )
+
+  multi_interactions <- dbGetQuery(con, query)
+
+  query <- glue_sql("SELECT m.*, t.* FROM networkMetrics m, tissues t 
+                    WHERE m.idTissue = {tissue} AND t.idTissue = {tissue}", .con = con)
+  
+  calculated_tissue_metrics <- dbGetQuery(con, query)
+  
+  return(list(multi_interactions = multi_interactions, tissueMetrics = calculated_tissue_metrics))
+}
+
+#* Buscar todas as interações de um tecido e retornar o dados para processamento no cliente
+#* @param prot Proteína inicial (para cálculo de métricas)
+#* @param layer Grau máximo de interação (1-4)
+#* @param tissue Nome do tecido (ex: "cortex")
+#* @post /single_query
+function(prot, tissue, layer) {
+  con <- database_connect()
+  on.exit(dbDisconnect(con), add = TRUE) # Garante o fechamento seguro da conexão
+  layer <- as.integer(layer)
+  # Buscar todas as interações do tecido
+  query <- glue_sql("
+    SELECT geneSymbol1, geneSymbol2, idTissue
+    FROM interactions
+    WHERE idTissue = {tissue}", .con = con)
+
+  all_interactions <- dbGetQuery(con, query)
+  
+  # Processar o grafo com igraph
+  if (is.data.frame(all_interactions) && nrow(all_interactions) > 0) {
+      edges <- all_interactions %>%
+      rename(from = geneSymbol1, to = geneSymbol2) %>%
+      mutate(pair = ifelse(from < to, paste(from, to, sep = "_"), paste(to, from, sep = "_"))) %>%
+      distinct(pair, .keep_all = TRUE) %>%
+      select(from, to)
+  
+  # Extrair o subgrafo baseado no grau de interação
+      subgraph_edges <- extract_subgraph_by_degree(edges, prot, 
+                                                     as.integer(layer))
+      all_edges <- edges
+  } else {
+        subgraph_edges <- data.frame(from = character(), to = character())
+        all_edges <- data.frame(from = character(), to = character())
+        
+  }
+  
+  # Obter os nomes das proteínas
+  vProteins <- unique(c(subgraph_edges$from, subgraph_edges$to))
+
+  # Buscar as metricas calculadas paras as proteinas
+  query <- glue_sql("
+    SELECT m.*, p.*
+    FROM proteinMetrics m, proteins p
+    WHERE p.geneSymbol IN ({vProteins*}) 
+      AND m.idTissue = {tissue} 
+      AND m.idProtein = p.idProtein", .con = con)
+  
+  all_proteins_metrics <- dbGetQuery(con, query)
+  
+  # Buscar as métricas calculadas para uma proteínas especifica
+  
+  query <- glue_sql("
+    SELECT m.*, p.*
+    FROM proteinMetrics m, proteins p
+    WHERE p.geneSymbol = {prot} 
+      AND m.idTissue = {tissue} 
+      AND m.idProtein = p.idProtein
+  ", .con = con)
+  
+  selected_proteins_metrics <- dbGetQuery(con, query)
+
+  # Buscar as métricas calculadas para o tecido
+  
+  query <- glue_sql("SELECT m.*, t.* FROM networkMetrics m, tissues t 
+                    WHERE t.idTissue = {tissue}
+                    AND m.idTissue = {tissue}", .con = con)
+  calculated_tissue_metrics <- dbGetQuery(con, query)
+  
+  return(list(
+    all_interactions = subgraph_edges,
+    all_tissue_edges = all_edges,
+    proteinMetrics = selected_proteins_metrics,
+    allProteinsMetrics = all_proteins_metrics,
+    tissueMetrics = calculated_tissue_metrics
+  ))
+}
---
+++ app/tissuePPI.R
@@ -0,0 +1,521 @@
+library(shiny)
+library(httr)
+library(jsonlite)
+library(visNetwork)
+library(ggraph)       # Excluir se não for usar
+library(ggplot2)      # Excluir se não for usar o ggraph
+library(dplyr)
+library(igraph)  
+library(tidygraph)
+library(shinycssloaders)
+
+# Função para chamar a API, para teste local descomente a linha com "localhost"
+api_base_url <- "http://tissueppi-api:8003"
+#api_base_url <- "http://localhost:8003"
+
+sqlite_path <- file.path(getwd(), "data", "interactions.sqlite")
+
+# Função para buscar dados da API
+fetch_from_api <- function(endpoint, params = NULL) {
+  url <- paste0(api_base_url, endpoint)
+  response <- tryCatch({
+    if (is.null(params)) {
+      GET(url)
+    } else {
+      GET(url, query = params)
+    }
+  }, error = function(e) {
+    return(NULL)
+  })
+  
+  if (is.null(response) || status_code(response) != 200) {
+    return(NULL)
+  }
+  
+  content(response, "parsed", simplifyDataFrame = TRUE)
+}
+
+# Carregar os códigos dos tecidos
+tissue_input <- fetch_from_api("/tissue_list")
+
+# Carregar os códigos das proteínas
+protein_input <- fetch_from_api("/protein_list")
+
+ui_page <- reactiveVal("home")
+
+ui <- uiOutput("main_ui")
+
+server <- function(input, output, session) {
+
+  # Criar o grafo para exibição no visNetwork
+  createGraph <- function(arestas, metricas) {
+     grafo <- graph_from_data_frame(arestas, directed = FALSE)
+     paleta <- colorRampPalette(c("#1f77b4", "#d62728"))(100)
+     metricas$color <- color_map(metricas$degree, paleta)
+     metricas <- metricas %>% mutate(size = (metricas$clustering*100))
+     nodesGraph <- data.frame(proteinName = V(grafo)$name, stringsAsFactors = FALSE)
+     nodesGraph <- nodesGraph %>% left_join(metricas, by = c("proteinName" = "geneSymbol"))
+  
+  # Converter o igraph para um objeto tbl_graph
+     g_tidy <- as_tbl_graph(grafo)
+  
+  # Fazer o join direto como se fosse uma tabela
+     g_tidy <- g_tidy %>%
+       activate(nodes) %>%
+       left_join(nodesGraph, by = c("name" = "proteinName"))
+     grafo <- as.igraph(g_tidy)
+     coords <- layout_with_fr(grafo) * 100
+     V(grafo)$x <- coords[, 1]
+     V(grafo)$y <- coords[, 2]
+     return(grafo)
+  }
+  criarGrafo <- function(arestas, metricas){
+#    grafo <- graph_from_data_frame(arestas, directed = FALSE)
+    grafo <- as_tbl_graph(arestas, directed = FALSE) %>% 
+      # Calcula o grau (número de conexões) para cada nó e salva em uma variável
+      mutate(grau = centrality_degree(), clusterizacao = local_transitivity())
+    
+    ggraph(grafo, layout = "stress") + 
+      # Arestas finas e discretas
+      geom_edge_link(color = "blue", alpha = 0.3, width = 0.3) + 
+      # Mapeia TAMANHO ao grau e COR à clusterização
+      geom_node_point(aes(size = grau, color = clusterizacao)) + 
+      # Escala de cores contínua (ex: plasma ou viridis)
+      scale_color_viridis_c(
+        option = "plasma", 
+        name = "Coef. Clusterização",
+        limits = c(0, 1) # Força a legenda a exibir a escala real de 0 a 1
+      ) + 
+      # Escala de tamanho dos nós físicos
+      scale_size_continuous(range = c(2, 8), name = "Grau (Conexões)") +
+      # Nomes dos nós sem sobreposição
+      geom_node_text(aes(label = name), repel = TRUE, size = 2.5, max.overlaps = 20) + 
+      labs(
+        title = "Tissueppi version 1.0",
+        subtitle = "Protein ACTB2 network on tissue bladder",
+        caption = "Source: Tissueppi version 1.0"
+      ) +
+      # Tema limpo de rede
+      theme_void() +
+      theme(
+        legend.position = "right",
+        plot.background = element_rect(fill = "white", color = NA)
+      )
+}
+  # Calcular a escala para o tamaho do nó.
+  scale_size <- function(x) {
+    if (all(x == 0)) return(rep(5, length(x)))
+    scaled <- log10(x) * 15
+    scaled
+  }
+  # Definir a cor baseada no grau.
+  color_map <- function(x, paleta) {
+    min_val <- 1
+    max_val <- 10000
+    
+    # Evita log de zero ou negativo
+    x <- pmax(pmin(x, max_val), min_val) 
+    
+    # Aplica o log na base 10 (escala de 0 a 4, log10(10000) = 4)
+    log_x <- log10(x)
+    log_min <- log10(min_val)
+    log_max <- log10(max_val)
+    
+    # Normaliza na escala logarítmica e mapeia para 1:100
+    indices <- round(((log_x - log_min) / (log_max - log_min)) * 99) + 1
+    return(paleta[indices])
+  }
+  
+  output$protein_selector_ui <- renderUI({
+    selectInput("protein", "", 
+                   choices = protein_input$geneSymbol,
+#                    choices = setNames(protein_input$idProtein, protein_input$geneSymbol),
+                    selected = "FUCA2")
+  })
+  
+  output$tissue_selector_ui <- renderUI({
+    selectInput("tissue", "",
+                   choices = setNames(tissue_input$idTissue, tissue_input$descTissue),
+                   selected = "BLDDER")
+  })
+  
+  output$multiple_protein_selector_ui <- renderUI({
+    selectizeInput("protein_multi", "", 
+                   choices = protein_input$geneSymbol, multiple = TRUE)
+  })
+  
+  # Dados das proteínas  
+  output$protein_degree_ui <- renderText({
+    datalist_rv()$proteinMetrics$degree
+  })
+  
+  output$protein_betweenness_ui <- renderText({
+    datalist_rv()$proteinMetrics$betweenness
+  })
+  
+  output$protein_closeness_ui <- renderText({
+    datalist_rv()$proteinMetrics$closeness
+  })
+  
+  output$protein_count_ui <- renderText({
+    datalist_rv()$proteinMetrics$degree
+  })
+  
+  output$protein_name_ui <- renderText({
+    datalist_rv()$proteinMetrics$geneSymbol
+  })
+  
+  output$protein_geneid_ui <- renderText({
+    datalist_rv()$proteinMetrics$idGene
+  })
+  
+  output$protein_interactions_ui <- renderText({
+    nrow(datalist_rv()$all_interactions)
+  })
+
+  output$protein_clustering_ui <- renderText({
+    datalist_rv()$proteinMetrics$clustering
+  })
+
+  # Dados dos tecidos
+  output$tissue_name_ui <- renderText({
+    datalist_rv()$tissueMetrics$descTissue
+  })
+  
+  output$protein_diameter_ui <- renderText({
+    datalist_rv()$tissueMetrics$diameter
+  })
+  
+  output$tissue_interactions_ui <- renderText({
+    datalist_rv()$tissueMetrics$totalEdges
+  })
+  
+  output$tissue_distance_ui <- renderText({
+    datalist_rv()$tissueMetrics$distancia
+  })
+  
+  output$tissue_proteins_ui <- renderText({
+    datalist_rv()$tissueMetrics$totalNodes
+  })
+  
+  output$tissue_diameter_ui <- renderText({
+    datalist_rv()$tissueMetrics$diameter
+  })
+  
+  output$tissue_density_ui <- renderText({
+    datalist_rv()$tissueMetrics$density
+  })
+
+  output$tissue_clustering_ui <- renderText({
+    datalist_rv()$tissueMetrics$clustering
+  })
+
+    output$tissue_degree_ui <- renderText({
+    datalist_rv()$tissueMetrics$avgDegree
+  })
+  
+  datalist_rv <- reactiveVal(NULL)
+  graph_edges_rv <- reactiveVal(NULL)  
+
+  output$download_edges <- downloadHandler(
+
+    filename = function() {
+      paste0("protein_", datalist_rv()$proteinMetrics$geneSymbol, "_", datalist_rv()$tissueMetrics$idTissue, "_edgelist_", Sys.Date(), ".csv")
+    },
+    content = function(file) {
+      edges <- graph_edges_rv()
+      if (!is.data.frame(edges) || nrow(edges) == 0) {
+        div(
+            style = "padding: 8px 14px;
+                   color: #b71c1c;
+                   background-color: #fdecea;
+                   border: 1px solid #f5c6cb;
+                   border-radius: 6px;
+                   font-size: 16px;
+                   font-family: Arial, sans-serif;",
+            "No interactions found !"
+        )
+      } else {
+        write.csv(edges, file, row.names = FALSE)
+      }
+    }
+  )
+  # Rever o sql que traz esses dados, está trazendo só os dados dos vizinhos.
+  output$download_tissue_edges <- downloadHandler(
+
+    filename = function() {
+      paste0("tissue_",datalist_rv()$tissueMetrics$idTissue, "_edgelist_", Sys.Date(), ".csv")
+    },
+    content = function(file) {
+      edges <- datalist_rv()$all_tissue_edges
+      if (!is.data.frame(edges) || nrow(edges) == 0) {
+        div(
+          style = "padding: 8px 14px;
+                   color: #b71c1c;
+                   background-color: #fdecea;
+                   border: 1px solid #f5c6cb;
+                   border-radius: 6px;
+                   font-size: 16px;
+                   font-family: Arial, sans-serif;",
+          "No interactions found !"
+        )
+      } else {
+        write.csv(edges, file, row.names = FALSE)
+      }
+    }
+  )
+
+  output$download_network <- downloadHandler(
+
+    filename = function() {
+      paste0("network_",datalist_rv()$proteinMetrics$geneSymbol, "_", datalist_rv()$tissueMetrics$idTissue, "_", Sys.Date(), ".html")
+    },
+      content = function(file) {
+        edges <- graph_edges_rv()
+        if (!is.data.frame(edges) || nrow(edges) == 0) {
+        div(
+            style = "padding: 8px 14px;
+                   color: #b71c1c;
+                   background-color: #fdecea;
+                   border: 1px solid #f5c6cb;
+                   border-radius: 6px;
+                   font-size: 16px;
+                   font-family: Arial, sans-serif;",
+            "No interactions found !"
+            
+        )
+      } else {
+           graphTitle <- paste0(datalist_rv()$proteinMetrics$geneSymbol, " protein network ")
+           graphSubTitle <- paste0("Tissue ", datalist_rv()$tissueMetrics$idTissue)
+           nodesMetrics <- datalist_rv()$allProteinsMetrics
+           grafo <- createGraph(edges, nodesMetrics)
+           grafoVis <- toVisNetworkData(grafo)
+           vis <- visNetwork(nodes = grafoVis$nodes, edges = grafoVis$edges,
+                      main = graphTitle,
+                      submain = graphSubTitle) %>% 
+           visNetwork::visOptions(highlightNearest = FALSE, nodesIdSelection = FALSE) %>%
+           visIgraphLayout() %>% 
+           visInteraction(
+             dragNodes = FALSE,       # Impede arrastar os nós
+             dragView = FALSE,        # Impede arrastar a tela de fundo
+             zoomView = FALSE,        # Desativa o zoom (roda do mouse)
+             selectable = FALSE,      # Impede selecionar nós/arestas com o clique
+             hover = FALSE            # Desativa efeitos ao passar o mouse por cima
+           ) %>% 
+             visPhysics(enabled = FALSE) # Desativa a física
+           visNetwork::visSave(vis, file = file)
+      }
+
+    }
+  )
+  
+  # Renderizar o HTML
+  output$main_ui <- renderUI({
+    nav_buttons <- list(
+      home = actionButton("home", "TissuePPI"),
+      go_home = actionButton("go_home", "Home"),
+      go_about = actionButton("go_about", "About"),
+      go_search = actionButton("single_search", "Search"),
+      go_help = actionButton("go_help", "Help"),
+      multiple_search = actionButton("multiple_search", "Multiple Proteins"),
+      single_search = actionButton("single_search", "Single Protein"),
+      search_single_protein = actionButton("search_single_protein", "Search"),
+      search_multiple_protein = actionButton("search_multiple_protein", "Search")
+    )
+    
+
+    output$graph_ui_single <- renderUI({
+      req(input$search_single_protein)
+      edges <- datalist_rv()$all_interactions
+      
+      if (!is.data.frame(edges) || is.null(nrow(edges))) {
+        div(
+          style = "padding: 8px 14px;
+                   color: #b71c1c;
+                   background-color: #fdecea;
+                   border: 1px solid #f5c6cb;
+                   border-radius: 6px;
+                   font-size: 16px;
+                   font-family: Arial, sans-serif;",
+          "No interactions found !"
+
+        )
+      } else {
+        nodesMetrics <- datalist_rv()$allProteinsMetrics
+        grafo <- createGraph(edges, nodesMetrics)
+       V(grafo)$title <- paste0(
+          "Proteín: ", V(grafo)$name, "<br>",
+          "Degree: ", V(grafo)$degree, "<br>",
+          "Betweenness: ", V(grafo)$betweenness, "<br>",
+          "Coef. Clustering: ", V(grafo)$clustering
+        )
+        visIgraph(grafo) %>%
+          visEdges(arrows = "none") %>%
+          visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE)
+#        grafo <- criarGrafo(edges, nodesMetrics)
+              }
+    })
+
+# Alterar para usar a função createGraph  (Esta função ainda não foi implementada)      
+    output$graph_ui_multiple <- renderUI({
+      req(input$search_multiple_protein)
+      
+      edges <- graph_edges_rv()
+
+      if (!is.data.frame(edges) || nrow(edges) == 0) {
+        div(
+          style = "padding: 8px 14px;
+                   color: #b71c1c;
+                   background-color: #fdecea;
+                   border: 1px solid #f5c6cb;
+                   border-radius: 6px;
+                   font-size: 16px;
+                   font-family: Arial, sans-serif;",
+          "No interactions found !"
+          
+        )
+      } else {
+        nodes <- unique(c(edges$from, edges$to))
+        nodes_df <- data.frame(id = nodes, label = nodes)
+        
+        visNetwork::visNetwork(nodes_df, edges, width = "100%", main = " ") %>%
+          visNetwork::visEdges(arrows = "none") %>%
+          visNetwork::visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE) %>%
+          visPhysics(stabilization = TRUE) %>%
+          visNetwork::visLayout(randomSeed = 50)
+
+              }
+    })
+    
+    switch(
+      ui_page(),
+      home = htmlTemplate("www/home.html", !!!nav_buttons),
+      downloadFiles = htmlTemplate("www/download.html", !!!nav_buttons),
+      about = htmlTemplate("www/about.html", !!!nav_buttons),
+      single_search = htmlTemplate(
+        "www/single_search.html",
+        !!!nav_buttons,
+        multiple_search = nav_buttons$multiple_search,
+        tissue_selector = uiOutput("tissue_selector_ui"),
+        protein_selector = uiOutput("protein_selector_ui"),
+        protein_count = textOutput("protein_count_ui"),
+        protein_degree = textOutput("protein_degree_ui"),
+        protein_name = textOutput("protein_name_ui"),
+        protein_interactions = textOutput("protein_interactions_ui"),
+        protein_closeness = textOutput("protein_closeness_ui"),
+        protein_betweenness = textOutput("protein_betweenness_ui"),
+        protein_diameter_ui = textOutput("protein_diameter_ui"),
+        protein_geneid = textOutput("protein_geneid_ui"),
+        protein_clustering = textOutput("protein_clustering_ui"),
+        tissue_name = textOutput("tissue_name_ui"),
+        tissue_interactions = textOutput("tissue_interactions_ui"),
+        tissue_proteins = textOutput("tissue_proteins_ui"),
+        tissue_diameter = textOutput("tissue_diameter_ui"),
+        tissue_density = textOutput("tissue_density_ui"),
+        tissue_clustering = textOutput("tissue_clustering_ui"),
+        tissue_degree = textOutput("tissue_degree_ui"),
+        graph_interactions = uiOutput("graph_ui_single"),
+        downloadHTML = downloadButton("download_network", "Network (HTML)"),
+        downloadEDGES = downloadButton("download_edges", "Edge List (CSV)"),
+        downloadTISSUE = downloadButton("download_tissue_edges", "Tissue's Edge (CSV)")
+      ),
+      help = htmlTemplate("www/help.html", !!!nav_buttons),
+      multiple_search = htmlTemplate(
+        "www/multiple_search.html",
+        !!!nav_buttons,
+        multiple_protein_selector = uiOutput("multiple_protein_selector_ui"),
+        tissue_selector = uiOutput("tissue_selector_ui"),
+        tissue_name = textOutput("tissue_name_ui"),
+        tissue_interactions = textOutput("tissue_interactions_ui"),
+        tissue_proteins = textOutput("tissue_proteins_ui"),
+        tissue_diameter = textOutput("tissue_diameter_ui"),
+        tissue_density = textOutput("tissue_density_ui"),
+        tissue_clustering = textOutput("tissue_clustering_ui"),
+        tissue_degree = textOutput("tissue_degree_ui"),
+        graph_interactions = uiOutput("graph_ui_multiple"),
+        downloadHTML = downloadButton("download_network", "Download Network (HTML)"),
+        downloadEDGES = downloadButton("download_edges", "Download Edge List (CSV)")
+      )
+    )
+  })
+  
+  # Observadores de navegação
+  observeEvent(input$home, ui_page("home"))
+  observeEvent(input$go_home, ui_page("home"))
+  observeEvent(input$go_about, ui_page("about"))
+  observeEvent(input$go_search, ui_page("search"))
+  observeEvent(input$go_help, ui_page("help"))
+  observeEvent(input$single_search, ui_page("single_search"))
+  observeEvent(input$multiple_search, ui_page("multiple_search"))
+  
+  # Observador para pesquisa individual
+  observeEvent(input$search_single_protein, {
+    req(input$protein, input$tissue)
+    response <- POST(
+        url <- paste0(api_base_url, "/single_query"),
+        body = list(
+        prot = input$protein,
+        tissue = input$tissue,
+        layer = 1
+        
+      ),
+      encode = "json"
+    )
+    
+    if (http_type(response) != "application/json") {
+      showNotification("Error accessing the API", type = "error")
+      return()
+    }
+    
+    data <- content(response, as = "parsed", simplifyDataFrame = TRUE)
+    if (!is.null(data$error)) {
+      showNotification(paste("Error:", data$error), type = "error")
+      return()
+    }
+    
+    datalist_rv(data)
+    graph_edges_rv(data$all_interactions)
+  }, ignoreInit = FALSE)
+  
+  # Observadores para pesquisa múltipla
+  observeEvent(input$search_multiple_protein, {
+
+    req(input$protein_multi, input$tissue)
+    
+    response <- POST(
+      url <- paste0(api_base_url, "/multiple_query"),
+      body = list(
+        genes = paste(input$protein_multi, collapse = ","),
+        tissue = input$tissue
+      ),
+      encode = "json"
+    )
+    
+    if (http_type(response) != "application/json") {
+      showNotification("Error accessing the API", type = "error")
+      return()
+    }
+    
+    data <- content(response, as = "parsed", simplifyDataFrame = TRUE)
+    
+    if (!is.null(data$error)) {
+      showNotification(paste("Error:", data$error), type = "error")
+      return()
+    }
+    datalist_rv(data)
+    graph_edges_rv(data$multi_interactions)
+    
+    if (is.data.frame(graph_edges_rv())) {
+      edges <- graph_edges_rv() %>%
+        rename(from = geneSymbol1, to = geneSymbol2) %>%
+        mutate(pair = ifelse(from < to, paste(from, to, sep = "_"), paste(to, from, sep = "_"))) %>%
+        distinct(pair, .keep_all = TRUE) %>%
+        select(from, to)
+        graph_edges_rv(edges)
+    } else {
+            graph_edges_rv(data.frame(from = character(), to = character()))
+    }
+   })
+}
+
+shinyApp(ui, server)
